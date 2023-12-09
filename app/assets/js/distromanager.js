573f8f593652b6f8569f9e2b278382ddf6a0e0d8
xjqh12 <xjqh12@gmail.com>
2023-12-09 17:28:36 +0900

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -5,7 +5,7 @@ const ConfigManager = require('./configmanager')
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
 //exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://kwh4967.synology.me/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://kwh4967.synology.me/nebulaFabric/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
